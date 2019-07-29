2ece6fd287828050dffbfc551698f5494920f8c7
Andy Cary <andy@andycary.com>
2019-07-29 17:24:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,7 +20,9 @@ currentLine(katzDeliLine); // "The line is currently: 1. Kent, 2. Matz"
 
 // Function: takeANumber
 function takeANumber(katzDeliLine, name) {
-
+  var position;
+  katzDeliLine.push(name);
+  
   return "Welcome, " + name + ". You are number 1 in line.";
 }
 
